42a23ea9dee9dba21091e71bb5ae19f516979b9c
Cody Nguyen <codyjimnguyen@gmail.com>
2019-08-15 11:03:00 -0600

    add git help menu

---
+++ main.rb
@@ -0,0 +1,32 @@
+class Git
+  def initialize
+    menu
+  end
+
+  def menu
+    puts "1) Enter git command"
+    puts "2) Exit"
+    print "> "
+    choice = gets.to_i
+    case choice
+    when 1
+      puts "Enter git command:"
+      print "> "
+      puts_git(gets.strip)
+      menu
+    when 2
+      exit
+    else
+      puts "Invalid Input"
+      sleep (2)
+      print `clear`
+      menu
+    end
+  end
+
+  def puts_git(cmd)
+    puts `git #{cmd} -h`
+  end
+end
+
+Git.new
